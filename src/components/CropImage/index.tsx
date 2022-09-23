4078b75f5ffe6992c795ae5056f6982b0a2d5334
ashishthakor1 <ashish.t@crestinfosystems.net>
2022-09-23 18:41:02 +0530

    UI completed

--- src/components/CropImage/index.tsx
+++ src/components/CropImage/index.tsx
@@ -1,12 +1,16 @@
+import { Slider, Typography, Button } from '@mui/material';
 import React, { useState } from 'react'
 
 const CropImage = () => {
     const [selectedFile, setSelectedFile] = useState<any>(null);
+    const [rotation, setRotation] = useState<number>(0);
+
     const fileChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
         if (event.target.files !== null)
             setSelectedFile(URL.createObjectURL(event?.target?.files[0]));
     }
     // console.log("selectedFile", selectedFile);
+    console.log("rotation", rotation);
     return (
         <>
             <input
@@ -16,7 +20,26 @@ const CropImage = () => {
                 onChange={(event) => fileChangeHandler(event)}
             />
             <div className='input-image'>
-                
+                <Typography
+                    variant="overline"
+                >
+                    Rotation
+                </Typography>
+                <Slider
+                    value={rotation}
+                    min={0}
+                    max={360}
+                    step={1}
+                    aria-labelledby="Rotation"
+                    onChange={(e, rotation) => setRotation(Number(rotation))}
+                />
+                <Button
+                    // onClick={showCroppedImage}
+                    variant="contained"
+                // color="primary"
+                >
+                    Show Result
+                </Button>
             </div>
         </>
     )
